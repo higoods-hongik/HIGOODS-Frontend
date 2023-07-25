a10889d4cb6d1dbac4946c834c64b71d4b438849
한규진 <david0218@naver.com>
2023-07-25 15:21:01 +0900

    chore : netlify 404 error, fallback 로딩

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,19 @@ import { ThemeProvider } from "./styles/theme";
 import NavBar from "./components/NavBar";
 import { Suspense } from "react";
 import { SyncLoader } from "react-spinners";
+import styled from "@emotion/styled";
 
 const App = () => {
   return (
     <ThemeProvider>
       <NavBar />
-      <Suspense fallback={<SyncLoader />}>
+      <Suspense
+        fallback={
+          <FallBackContainer>
+            <SyncLoader color="#E6E6E6" />
+          </FallBackContainer>
+        }
+      >
         <Outlet />
       </Suspense>
     </ThemeProvider>
@@ -17,3 +24,13 @@ const App = () => {
 };
 
 export default App;
+
+const FallBackContainer = styled.div`
+  width: 100vw;
+  height: calc(100vh - 120px);
+  height: calc(100svh - 120px);
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
