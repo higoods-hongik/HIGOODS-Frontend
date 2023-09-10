10f210941b3b2b8168e955d3da7b4724062c099f
한규진 <55226431+9yujin@users.noreply.github.com>
2023-09-10 22:31:44 +0900

    chore : 어드민 - 상세정보 입력 (#16)

--- src/pages/admin/Info/Detail.tsx
+++ src/pages/admin/Info/Detail.tsx
@@ -1,51 +1,51 @@
+import { css } from "@emotion/react";
 import styled from "@emotion/styled";
+import { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import Button from "~/components/atoms/Button";
 import { Txt } from "~/components/atoms/Txt";
 import { FlexBox } from "~/components/layout/FlexBox";
 import { GridBox } from "~/components/layout/GridBox";
 import { Spacing } from "~/components/layout/Spacing";
+import TextEditor from "~/components/text-editor";
 
 export const Detail = () => {
   const navigate = useNavigate();
   const { id } = useParams<{ id: string }>();
+  const [content, setContent] = useState(" ");
 
   return (
     <>
       <FlexBox gap={12} justify={"flex-start"}>
-        <Txt typo="heading.1">상품 정보 이력</Txt>
+        <Txt typo="heading.1">상세 정보 이력</Txt>
         <Txt typo="heading.4" color="red">
-          (2/3 단계)
+          (3/3 단계)
         </Txt>
       </FlexBox>
       <Spacing size={24} />
       <GridBox gridTemplateColumns={"auto 252px"} gap={84}>
         <그리드_왼쪽영역>
-          <Txt typo="heading.4" required as="div">
-            상품명을 입력해주세요.
+          <Txt typo="heading.4" as="div">
+            공구 프로젝트에 대한 설명을 작성해주세요.
           </Txt>
+          <Spacing size={22} />
+          <div css={css({ position: "relative", zIndex: -1, width: 648 })}>
+            <TextEditor setContent={setContent} content={content} />
+          </div>
         </그리드_왼쪽영역>
         <그리드_오른쪽영역>
-          <Spacing size={102} />
+          <Spacing size={44} />
           <NoticeBox>
             <Txt color="red" typo="heading.4" as="div">
-              옵션 설정 가이드
+              설명 작성 가이드
             </Txt>
             <Spacing size={12} />
             <Txt typo="body.2">
-              상품의 종류가 여러 개인가요?
+              진행하려는 공구에 대한 설명을
               <br />
-              옵션을 입력해서 공구 참여자들이
+              적어주세요. 자세하게 쓸수록 구매에
               <br />
-              원하는 선택지를 고르게 할 수 있고,
-              <br />
-              선택지별로 가격 설정도 가능해요.
-            </Txt>
-            <Spacing size={12} />
-            <Txt typo="body.2" color="grey4">
-              ex. 의류 상품 {">"} '사이즈' 옵션 추가
-              <br />
-              {"        >"} S, M, L 선택지 추가
+              도움이 돼요.
             </Txt>
           </NoticeBox>
         </그리드_오른쪽영역>
@@ -61,7 +61,7 @@ export const Detail = () => {
           width={312}
           onClick={() => navigate(`/admin/${id}/info/detail`)}
         >
-          다음으로 (2/3)
+          다음으로
         </Button>
       </FlexBox>
       <Spacing size={100} />
